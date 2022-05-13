eb820f88a9369bac2ab3dd796324a0fb24f9d839
“bnyte” <bnytezz@gmail.com>
2022-05-13 19:17:55 +0800

    chore: WordCountDriver

--- example/java/hadoop/wordcount/src/main/java/com/bnyte/mapreduce/driver/WordCountDriver.java
+++ example/java/hadoop/wordcount/src/main/java/com/bnyte/mapreduce/driver/WordCountDriver.java
@@ -11,6 +11,9 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author bnyte
@@ -19,10 +22,18 @@ import java.io.IOException;
 public class WordCountDriver {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        if (null == args) throw new RuntimeException("output file cannot be empty");
+        List<String> argList = Arrays.asList(args);
+        if (argList.size() < 2) {
+            throw new RuntimeException("output file cannot be empty");
+        }
+
+
         // 指定hadoop_home
-        System.setProperty("hadoop.home.dir", "D:\\software\\hadoop-3.1.0");
+//        System.setProperty("hadoop.home.dir", "D:\\software\\hadoop-3.1.0");
         // 加载hadoop.dll文件
-        System.load("D:\\software\\hadoop-3.1.0\\bin\\hadoop.dll");
+//        System.load("D:\\software\\hadoop-3.1.0\\bin\\hadoop.dll");
         // 获取job实例对象
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf);
@@ -44,9 +55,11 @@ public class WordCountDriver {
         job.setOutputValueClass(LongWritable.class);
 
         // 指定输入文件和输出文件
-        FileInputFormat.setInputPaths(job, new Path("D:\\hadoop\\example\\input\\wordcount.txt"));
-        FileOutputFormat.setOutputPath(job, new Path("D:\\hadoop\\example\\output"));
+//        FileInputFormat.setInputPaths(job, new Path("D:\\hadoop\\example\\input\\wordcount.txt"));
+//        FileOutputFormat.setOutputPath(job, new Path("D:\\hadoop\\example\\output"));
 
+        FileInputFormat.setInputPaths(job, new Path(argList.get(0)));
+        FileOutputFormat.setOutputPath(job, new Path(argList.get(1)));
         // 提交job
         job.waitForCompletion(true); // 可以看到更详细的运行日志
         // 或者submit
