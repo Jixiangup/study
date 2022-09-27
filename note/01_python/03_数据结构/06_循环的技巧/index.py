619a27d78131077f3b86545b7e163b4766f902dd
bnyte <bnytezz@gmail.com>
2022-09-27 12:04:52 +0800

    添加python的循环的使用的注释

--- note/01_python/03_数据结构/06_循环的技巧/index.py
+++ note/01_python/03_数据结构/06_循环的技巧/index.py
@@ -28,6 +28,7 @@ print(new_basket)
 raw_data = [56.2, float('NaN'), 51.7, 55.3, 52.5, float('NaN'), 47.8]
 filtered_data = []
 for value in raw_data:
+    # 如果不为nan
     if not math.isnan(value):
         filtered_data.append(value)
 print(filtered_data)
